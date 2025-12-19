ac6a85a551d75a411f1358b71c565da7ee51197b
CalistDsouza <dsouzacalist2001@gmail.com>
2025-12-19 15:44:45 -0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Calist Dsouza on 2025-12-19.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
